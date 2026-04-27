22a9487c6a56fcca27635b127d233a0068d21e2e
ris <rissu.ntk@gmail.com>
2026-04-27 19:35:53 +0700

    Luminescence
    Signed-off-by: ris <rissu.ntk@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Euphoria Kernel for X01BD by Rissu @ rsukrnlprjkt
+kernel.string=Luminescence Kernel for X01BD by Rissu @ rsukrnlprjkt
 do.devicecheck=1
 do.modules=0
 do.systemless=1
