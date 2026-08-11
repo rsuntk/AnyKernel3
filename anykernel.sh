1baf6499adc0cf09db1ca2e0d5294ccc8a967613
rsuntk <90097027+rsuntk@users.noreply.github.com>
2026-08-11 13:27:21 +0700

    Update kernel string in anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Luminescence Kernel for X01BD by Rissu @ rsukrnlprjkt
+kernel.string=VivLOS Kernel for X01BD by Rissu @ rsukrnlprjkt
 do.devicecheck=1
 do.modules=0
 do.systemless=1
